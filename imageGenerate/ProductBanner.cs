e3e900970c329fec9bbdec743cbd4f0edba6665b
q <q>
2020-02-23 23:11:02 +0300

    initial

---
+++ imageGenerate/ProductBanner.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace imageGenerate
+{
+    public class ProductBanner
+    {
+        public int Width { get; set; } = 300;
+        public int Height { get; set; } = 250;
+        public Color BackgroundColor { get; set; } = Color.FromArgb(255, 170, 85);
+        public Bitmap BackgroundImage { get; set; }
+        public Bitmap ProductImage { get; set; }
+        public string Title { get; set; } = "Title";
+        public string Price { get; set; } = "Price";
+
+        public Bitmap LastRendered { get; set; }
+
+        public void SimpleRender()
+        {
+            Bitmap result = null;
+            try
+            {
+                result = new Bitmap(Width, Height);
+                var g = Graphics.FromImage(result);
+                g.Clear(BackgroundColor);
+
+                if (BackgroundImage != null)
+                {
+                    var x = (Width - BackgroundImage.Width) / 2;
+                    var y = (Height - BackgroundImage.Height) / 2;
+
+                    g.DrawImage(BackgroundImage, new Point(x, y));
+                }
+
+                if (ProductImage != null)
+                {
+                    var x = (Width - ProductImage.Width) >> 1;
+                    var y = (Height - ProductImage.Height) >> 1;
+
+                    g.DrawImage(ProductImage, new Point(x, y));
+                }
+
+                using (var font = new Font("Times New Roman", 16))
+                {
+                    using (var stringFormat = new StringFormat())
+                    {
+                        stringFormat.LineAlignment = StringAlignment.Center;
+                        stringFormat.Alignment = StringAlignment.Center;
+
+                        var titleRect = new Rectangle(1, 1, Width - 1, Height / 3 - 1);
+                        //g.DrawRectangle(new Pen(Color.Red, 1), titleRect);
+                        g.DrawString(Title, font, Brushes.White, titleRect, stringFormat);
+
+                        var priceRect = new Rectangle(1, 2 * Height / 3, Width - 1, Height / 3 - 1);
+                        //g.DrawRectangle(new Pen(Color.Red, 1), priceRect);
+                        g.DrawString(Price, font, Brushes.White, priceRect, stringFormat);
+                    }
+                }
+            }
+            catch (System.Exception) { }
+
+            if (LastRendered != null)
+                LastRendered.Dispose();
+
+            LastRendered = result;
+        }
+
+        void Save(string fileFullPath, ImageFormat imageFormat)
+        {
+            if (LastRendered == null)
+                return;
+
+            try
+            {
+                LastRendered.Save(fileFullPath, imageFormat);
+            }
+            catch (System.Exception) { }
+        }
+        public void SavePng(string fileFullPath) => Save(fileFullPath, ImageFormat.Png);
+        public void SaveJpg(string fileFullPath) => Save(fileFullPath, ImageFormat.Jpeg);
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using imageGenerate;
+using System.Drawing;
+//using System.Drawing.Imaging;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var backgroundImage = new Bitmap("back.jpg");
+            var washingBoots = new Bitmap("washing-boots.jpg");
+
+            var productBanner = new ProductBanner
+            {
+                BackgroundColor = Color.FromArgb(51, 63, 85),
+                //BackgroundImage = backgroundImage,
+                ProductImage = washingBoots,
+                Title = "Washing Boots",
+                Price = "123.45 TL"
+            };
+            productBanner.SimpleRender();
+            productBanner.SavePng("product-banner.png");
+            productBanner.SaveJpg("product-banner.jpg");
+        }
+    }
+}
